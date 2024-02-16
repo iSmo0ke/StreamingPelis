bce1919f34d09f6767ee73efab32cc6391019e3b
iSmo0ke <jesus.alt.2004@gmail.com>
2024-02-15 18:31:56 -0600

    Act1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/shared/header';
+import Footer from './components/shared/footer';
+import LoadingScreen from './components/screens/loadingScreen';
+import ErrorScreen from './components/screens/errorScreen';
+import Home from './components/screens/home';
+import Series from './components/screens/series';
+import Movies from './components/screens/movies';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/series" component={Series} />
+          <Route exact path="/movies" component={Movies} />
+          <Route path="/loading" component={LoadingScreen} />
+          <Route path="/error" component={ErrorScreen} />
+          <Route component={ErrorScreen} /> {/* Ruta predeterminada para manejar rutas no encontradas */}
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
